218ce47bccb67947874ea2b024ed480222e4d4a5
Karanpal Singh <singhkaranpal81@gmail.com>
2020-12-23 14:41:31 +0530

    Changed deps2

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json({ type : '*/*'}));
 router(app);
 app.use(express.static(path.join(__dirname,'client','build')))
 app.get('*',(req,res) => {
-    res.sendFile(path.join(__dirname,'client','build','index.html'))
+    res.sendFile('index.html')
 })
 
 // Server setup
